5b6c4738b5367a975271ce412a8d42e64c731d6c
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-03 17:54:32 -0300

    Use bit to compute power of two

--- ruby/counter_game.rb
+++ ruby/counter_game.rb
@@ -13,3 +13,15 @@ def counter_game(n, is_louise = true)
 
   counter_game(n, !player)
 end
+
+
+def is_power_of_two?(n)
+  n & (n - 1) == 0
+end
+# This expression works because when you subtract 1 from a power of two, all the bits up to the single 1-bit in the power of two become 1. So, when you perform a bitwise AND between the original number and the number minus one, the result should be 0.
+
+# For example, let's say n = 8 (which is a power of two):
+n = 1000 (binary)
+n - 1 = 0111 (binary)
+
+n & (n - 1) = 1000 & 0111 = 0000 (binary)
