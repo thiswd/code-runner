b75c62fa098250af9a68bbe3bd61d62223fc43ad
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-17 17:36:20 -0300

    Has cycle code

---
+++ ruby/has_cycle.rb
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode
+#   attr_accessor :val, :next
+#   def initialize(val)
+#       @val = val
+#       @next = nil
+#   end
+# end
+
+def has_cycle(head)
+  return false if head.nil? || head.next.nil?
+
+  slow = head
+  fast = head.next
+
+  while slow != fast
+    return false if fast.nil? || fast.next.nil?
+
+    slow = slow.next
+    fast = fast.next.next
+  end
+
+  true
+end
