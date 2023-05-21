ede72f887fb46c89e7006cc7e7b5f0677331ab38
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-20 22:35:02 -0300

    More solutions for avarage levels

--- ruby/average_of_levels.rb
+++ ruby/average_of_levels.rb
@@ -22,3 +22,48 @@ def average_of_levels(root)
 
   result
 end
+
+def average_of_levels(root)
+  return [] if root.nil?
+
+  sum = []
+  count = []
+  stack = [[root, 0]]
+
+  while !stack.empty?
+    node, level = stack.pop
+
+    sum[level] = (sum[level] || 0) + node.val
+    count[level] = (count[level] || 0) + 1
+
+    if node.left
+      stack.push([node.left, level + 1])
+    end
+
+    if node.right
+      stack.push([node.right, level + 1])
+    end
+  end
+
+  sum.each_with_index.map { |s, i| s.to_f / count[i] }
+end
+
+def average_of_levels(root)
+  return [] if root.nil?
+
+  sum = []
+  count = []
+
+  def dfs(node, level, sum, count)
+    return unless node
+
+    sum[level] = (sum[level] || 0) + node.val
+    count[level] = (count[level] || 0) + 1
+
+    dfs(node.left, level + 1, sum, count)
+    dfs(node.right, level + 1, sum, count)
+  end
+
+  dfs(root, 0, sum, count)
+  sum.each_with_index.map { |s, i| s.to_f / count[i] }
+end
