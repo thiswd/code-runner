1fc523e43a41595aad21e73a9390d7b83f7c7958
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-23 20:57:27 -0300

    TwoSum solution

---
+++ javascript/two_sum.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
+
+let arr = [1, 2, 3, 4, 6];
+let target = 6;
+console.log(twoSum(arr, target));
