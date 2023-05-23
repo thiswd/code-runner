860cb6751468c121c8db6d14f23dd07d65e31e68
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-23 11:49:53 -0300

    Other solution for is cousins in binary tree

--- ruby/is_cousins_in_binary_tree.rb
+++ ruby/is_cousins_in_binary_tree.rb
@@ -26,3 +26,22 @@ def is_cousins(root, x, y)
 
   found.size == 2 && found.values.uniq.size == 1 ? true : false
 end
+
+def is_cousins(root, x, y)
+  parents = {}
+  levels = {}
+
+  def dfs(node, parent, level, parents, levels)
+    return unless node
+
+    parents[node.val] = parent
+    levels[node.val] = level
+
+    dfs(node.left, node.val, level + 1, parents, levels)
+    dfs(node.right, node.val, level + 1, parents, levels)
+  end
+
+  dfs(root, nil, 0, parents, levels)
+
+  levels[x] == levels[y] && parents[x] != parents[y]
+end
