cee557400dd96a38a24892e33e93ae08772ee11b
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-25 18:56:54 -0300

    Two pointers solution

---
+++ ruby/two_sum.rb
@@ -0,0 +1,31 @@
+def two_sum(nums, target)
+  head = 0
+  tail = nums.length - 1
+
+  while head < tail
+    sum = nums[head] + nums[tail]
+
+    if sum == target
+      return [head, tail]
+    elsif sum < target
+      head += 1
+    else
+      tail -= 1
+    end
+  end
+end
+
+def two_sum(nums, target)
+  cache = {}
+  index = 0
+
+  while index < nums.length
+    remaining = target - nums[index]
+    if num = cache[remaining]
+      return [num, index]
+    else
+      cache[nums[index]] = index
+    end
+    index += 1
+  end
+end
