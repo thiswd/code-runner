0fbef462300e0859707caf57cfd12512d63eb6d2
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-26 20:06:10 -0300

    Find disappeared number different solutions

--- ruby/find_disappeared_numbers.rb
+++ ruby/find_disappeared_numbers.rb
@@ -17,3 +17,23 @@ def find_disappeared_numbers(nums)
   end
   missing_numbers
 end
+
+def find_disappeared_numbers(nums)
+  all_nums = Set.new(1..nums.size)
+  nums.each { |num| all_nums.delete(num) }
+  all_nums.to_a
+end
+
+def find_disappeared_numbers(nums)
+  counts = Array.new(nums.size + 1, 0)
+  nums.each { |num| counts[num] += 1 }
+  counts.each_index.select { |i| counts[i] == 0 && i != 0 }
+end
+
+def find_disappeared_numbers(nums)
+  nums.each do |num|
+    index = num.abs - 1
+    nums[index] = -nums[index].abs
+  end
+  nums.each_index.select { |i| nums[i] > 0 }.map { |i| i + 1 }
+end
