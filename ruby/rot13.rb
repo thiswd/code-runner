76af22dc9012fda4b6eae4a2fbabbc89fc2c0aae
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-28 18:37:32 -0300

    Rot13 solution

---
+++ ruby/rot13.rb
@@ -0,0 +1,22 @@
+def rot13(secret_messages)
+  letters = ("a".."z").to_a
+  letters_length = letters.length
+  rot13_map = {}
+
+	letters.each_with_index do |letter, index|
+    rot_index = index + 13
+    rot_index -= letters_length if rot_index >= letters_length
+    rot13_map[letter] = letters[rot_index]
+  end
+
+  secret_messages.map do |message|
+    i = 0
+    while i < message.length
+      if message[i] =~ /\S/
+        message[i] = rot13_map[message[i]]
+      end
+      i += 1
+    end
+    message
+  end
+end
