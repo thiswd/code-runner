f63df9daa266c41fa7d235a9a8a67db74e60f871
Thiago Sawada <tms1_tms2@hotmail.com>
2023-05-29 21:15:02 -0300

    Interval intersections solution

---
+++ ruby/interval_list_intersections.rb
@@ -0,0 +1,36 @@
+def interval_intersection(first_list, second_list)
+  i, j = 0, 0
+  intervals = []
+
+  while i < first_list.length && j < second_list.length
+    lo = [first_list[i].first, second_list[j].first].max
+    hi = [first_list[i].last, second_list[j].last].min
+
+    intervals << [lo, hi] if lo <= hi
+    first_list[i].last < second_list[j].last ? i += 1 : j += 1
+  end
+
+  intervals
+end
+
+
+def interval_intersection(first_list, second_list)
+  i, j = 0, 0
+  intervals = []
+
+  while i < first_list.length && j < second_list.length
+    interval = array_entries(first_list[i]) & array_entries(second_list[j])
+    intervals.push(interval.minmax) if interval.any?
+    first_list[i].last >= second_list[j].last ? j += 1 :  i += 1
+  end
+  intervals
+end
+
+def array_entries(arr)
+  (arr.first..arr.last).entries
+end
+
+first_list = [[0,2],[5,10],[13,23],[24,25]]
+second_list = [[1,5],[8,12],[15,24],[25,26]]
+
+puts interval_intersection(first_list, second_list).inspect
