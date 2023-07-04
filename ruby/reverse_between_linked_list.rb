d2045697b9a850d241355075a81c76e29b1dcb97
Thiago Sawada <tms1_tms2@hotmail.com>
2023-07-03 21:35:11 -0300

    reverse between linked list

---
+++ ruby/reverse_between_linked_list.rb
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode
+  attr_accessor :val, :next
+
+  def initialize(val = 0, next_node = nil)
+    @val = val
+    @next = next_node
+  end
+end
+
+# dummy->3->1->5->2->4
+
+def reverse_between(head, left, right)
+  return head if left == right
+
+  dummy = ListNode.new(0)
+  dummy.next = head
+  prev = dummy
+
+  (left - 1).times do
+    prev = prev.next
+  end
+
+  current = prev.next
+
+  (right - left).times do
+    temp = prev.next
+    prev.next = current.next
+    current.next = current.next.next
+    prev.next.next = temp
+  end
+
+  dummy.next
+end
